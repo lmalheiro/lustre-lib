42da0dba788b9adb333e15090c78bd3245b1d5e3
Luis Malheiro <luismalheiro@gmail.com>
2020-09-24 22:33:01 -0300

    Checkpoint.

--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -8,37 +8,70 @@ struct Evaluator<'a> {
     environment: &'a mut dyn Environment,
 }
 
+macro_rules! not_nil {
+    ($env:expr; $value:expr) => {
+        !Rc::ptr_eq($value, &$env.get_nil())
+    };
+}
+
 impl<'a> Evaluator<'a> {
     pub fn new(environment: &'a mut dyn Environment) -> Self {
         Evaluator { environment }
     }
 
     pub fn eval(&self, obj: Rc<Option<Object>>) -> Rc<Option<Object>> {
-        if Rc::ptr_eq(&obj, &self.environment.get_nil()) {
-            return self.environment.get_nil();
-        } else {
-            if let Object::Cons(car, cdr) = obj
-                .as_ref()
-                .as_ref()
-                .expect("Invalid 'None' object. It should have matched the 'nil'.")
-            {
-                operators::apply(
-                    self.eval(car.clone()),
-                    self.eval_list(cdr.clone()),
-                    self.environment,
-                )
-            } else {
-                obj
+        match obj.as_ref() {
+            None => self.environment.get_nil(),
+            Some(Object::Cons(car, cdr)) => {
+                if let Some(Object::Symbol(s)) = car.as_ref() {
+                    if s == "if" {
+                        let (car1, cdr) = operators::destructure_list(cdr.clone());
+                        let (car2, cdr) = operators::destructure_list(cdr);
+                        let (car3, _) = operators::destructure_list(cdr);
+                        let test = self.eval(car1);
+                        if not_nil!(self.environment; &test) {
+                            self.eval(car2)
+                        } else {
+                            self.eval(car3)
+                        }
+                    } else {
+                        operators::apply(
+                            self.eval(car.clone()),
+                            self.eval_list(cdr.clone()),
+                            self.environment,
+                        )
+                    }
+                } else {
+                    operators::apply(
+                        self.eval(car.clone()),
+                        self.eval_list(cdr.clone()),
+                        self.environment,
+                    )
+                }
             }
+            _ => obj,
         }
+        // if Rc::ptr_eq(&obj, &self.environment.get_nil()) {
+        //     return self.environment.get_nil();
+        // } else {
+        //     if let Object::Cons(car, cdr) = obj
+        //         .as_ref()
+        //         .as_ref()
+        //         .expect("Invalid 'None' object. It should have matched the 'nil'.")
+        //     {
+        //         operators::apply(
+        //             self.eval(car.clone()),
+        //             self.eval_list(cdr.clone()),
+        //             self.environment,
+        //         )
+        //     } else {
+        //         obj
+        //     }
+        // }
     }
 
     fn eval_list(&self, obj: Rc<Option<Object>>) -> Rc<Option<Object>> {
-        macro_rules! not_nil {
-            ($env:expr; $value:expr) => {
-                !Rc::ptr_eq($value, &$env.get_nil())
-            };
-        }
+        
 
         if not_nil!(self.environment; &obj) {
             if let Some(Object::Cons(car, cdr)) = obj.as_ref() {
@@ -87,7 +120,8 @@ mod tests {
 
     #[test]
     fn eval_test_2() {
-        let input = "(+ 1000 1000 (+ 10 10) (- 0 100 ))";
+        //let input = "(+ 1000 1000 (+ 10 10) (- 0 100 ))";
+        let input = "(if (< 20 20) (if (> 30 20) \"TRUE-TRUE\" \"TRUE-FALSE\") \"FALSE\")";
         let tokenizer = reader::tokenizer::Tokenizer::new(Cursor::new(input).bytes());
         let mut environment = environment::Environment::new();
         operators::initialize_operators(&mut environment);
@@ -98,8 +132,9 @@ mod tests {
         if let Some(_) = value.as_ref() {
             let evaluator = Evaluator::new(&mut environment);
             let result = evaluator.eval(value);
+            eprintln!("result: {:?}", result);
             if let Some(obj) = result.as_ref() {
-                eprintln!("result: {}", obj);
+                eprintln!("obj: {}", obj);
             }
         } else {
             panic!("Ooops! Not an object...")
--- src/evaluator/operators.rs
+++ src/evaluator/operators.rs
@@ -51,27 +51,87 @@ pub fn sub(obj: Rc<Option<Object>>, env: &dyn Environment) -> Rc<Option<Object>>
         } else {
             panic!("Should exist a list here...")
         }
-        
     }
     Rc::new(Some(Object::Integer(total)))
 }
 
-pub fn apply(function: Rc<Option<Object>>, obj: Rc<Option<Object>>, env: &dyn Environment) -> Rc<Option<Object>> {
-
-    match function.as_ref().as_ref().expect("Expecting a value, instead got nil or other None value.") {
-        Object::Function(_value) => unimplemented!(),
-        Object::Operator(f) => { f(obj, env) },
-        _ => { panic!("Expected operator or function.") }
+pub fn greater_than(obj: Rc<Option<Object>>, env: &dyn Environment) -> Rc<Option<Object>> {
+    let (car1, cdr) = destructure_list(obj);
+    let (car2, _) = destructure_list(cdr);
+    if integer_value(car1) > integer_value(car2) {
+        Rc::new(Some(Object::Integer(1)))
+    } else {
+        env.get_nil()
     }
 }
 
+pub fn less_than(obj: Rc<Option<Object>>, env: &dyn Environment) -> Rc<Option<Object>> {
+    let (car1, cdr) = destructure_list(obj);
+    let (car2, _) = destructure_list(cdr);
+    if integer_value(car1) < integer_value(car2) {
+        Rc::new(Some(Object::Integer(1)))
+    } else {
+        env.get_nil()
+    }
+}
+
+pub fn equal_to(obj: Rc<Option<Object>>, env: &dyn Environment) -> Rc<Option<Object>> {
+    let (car1, cdr) = destructure_list(obj);
+    let (car2, _) = destructure_list(cdr);
+    if integer_value(car1) == integer_value(car2) {
+        Rc::new(Some(Object::Integer(1)))
+    } else {
+        env.get_nil()
+    }
+}
+
+pub fn apply(
+    function: Rc<Option<Object>>,
+    obj: Rc<Option<Object>>,
+    env: &dyn Environment,
+) -> Rc<Option<Object>> {
+    match function
+        .as_ref()
+        .as_ref()
+        .expect("Expecting a value, instead got nil or other None value.")
+    {
+        Object::Function(_value) => unimplemented!(),
+        Object::Operator(f) => f(obj, env),
+        _ => panic!("Expected operator or function."),
+    }
+}
+
+pub fn destructure_list(list: Rc<Option<Object>>) -> (Rc<Option<Object>>, Rc<Option<Object>>) {
+    if let Some(Object::Cons(car, cdr)) = list.as_ref() {
+        (car.clone(), cdr.clone())
+    } else {
+        panic!("Not a list!");
+    }
+}
+
+pub fn symbol_value(sym: Rc<Option<Object>>) -> String {
+    if let Some(Object::Symbol(value)) = sym.as_ref() {
+        value.to_string()
+    } else {
+        panic!("Not a symbol!");
+    }
+}
+
+pub fn integer_value(int: Rc<Option<Object>>) -> i32 {
+    if let Some(Object::Integer(value)) = int.as_ref() {
+        *value
+    } else {
+        panic!("Not an integer!");
+    }   
+}
+
 pub fn initialize_operators(environment: &mut dyn Environment) {
+    environment.intern(String::from("+"), Rc::new(Some(Object::Operator(sum))));
+    environment.intern(String::from("-"), Rc::new(Some(Object::Operator(sub))));
+    environment.intern(String::from("="), Rc::new(Some(Object::Operator(equal_to))));
+    environment.intern(String::from("<"), Rc::new(Some(Object::Operator(less_than))));
     environment.intern(
-        String::from("+"),
-        Rc::new(Some(Object::Operator(sum))),
-    );
-    environment.intern(
-        String::from("-"),
-        Rc::new(Some(Object::Operator(sub))),
+        String::from(">"),
+        Rc::new(Some(Object::Operator(greater_than))),
     );
 }
